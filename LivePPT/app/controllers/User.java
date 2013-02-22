0fd287e9d28a3c1d424b888a5386f0d9fbb24159
WeiJie Li <fenrir1205@gmail.com>
2013-02-22 12:46:40 +0800

    抛弃JavaPlay预设首页，改为/help。将主页改为说明文字，添加User的controller

---
+++ LivePPT/app/controllers/User.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class User extends Controller {
+  
+    public static Result index() {
+        // return ok(index.render("MY new application is ready."));
+        return TODO;
+    }
+
+    public static Result login() {
+        // return ok(index.render("MY new application is ready."));
+        return TODO;
+    }
+
+    public static Result resign() {
+        // return ok(index.render("MY new application is ready."));
+        return TODO;
+    }    
+
+  
+}
