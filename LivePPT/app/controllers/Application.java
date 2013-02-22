71c8eb132e0104d16596271f66a3289f891bec48
WeiJie Li <fenrir1205@gmail.com>
2013-02-22 12:47:16 +0800

    上一个commits漏了上传Application.java

--- LivePPT/app/controllers/Application.java
+++ LivePPT/app/controllers/Application.java
@@ -8,12 +8,25 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
     }
 
+    public static Result help() {
+        return ok(help.render("MY new application is ready."));
+    }
   //   public static Result indext() {
 		// return okrender();
   //   }
-
+	public static Result tasks() {
+	    return TODO;
+	}
+	  
+	public static Result newTask() {
+		return TODO;
+	}
+	  
+  	public static Result deleteTask(Long id) {
+		return TODO;
+	}
   
 }
