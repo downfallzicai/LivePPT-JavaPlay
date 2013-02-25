0694e18a1eeb0facbd909f73a1a585b8eb8492dd
WeiJie Li <vincent.is.lwj@gmail.com>
2013-02-25 23:53:03 +0800

    把login的action和验证分离

--- LivePPT/app/controllers/UserAction.java
+++ LivePPT/app/controllers/UserAction.java
@@ -1,82 +1,74 @@
 package controllers;
 
-import play.*;
+import play.libs.Json;
 import play.mvc.*;
 import sun.misc.BASE64Encoder;
 
-import views.html.*;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ObjectNode;
+
+import models.UserAuthentication;
 import models.mysql.UserTable;
 
-
 public class UserAction extends Controller {
-	
-	public static String EncoderByMd5(String str) throws NoSuchAlgorithmException, 
-	UnsupportedEncodingException{
-	        //确定计算方法
-	        MessageDigest md5=MessageDigest.getInstance("MD5");
-	        BASE64Encoder base64en = new BASE64Encoder();
-	        //加密后的字符串
-	        String newstr=base64en.encode(md5.digest(str.getBytes("utf-8")));
-	        return newstr;
+
+
+
+	public static Result index() {
+		// return ok(index.render("MY new application is ready."));
+		return TODO;
 	}
-  
-    public static Result index() {
-        // return ok(index.render("MY new application is ready."));
-        return TODO;
-    }
 
-    public static Result login() {
-        // return ok(index.render("MY new application is ready."));
-        System.out.println("Here is the POST login process!");
-        final Map<String, String[]> values = request().body().asFormUrlEncoded();
-        final String username = values.get("username")[0];
-        final String password = values.get("password")[0];
-         
-        try {
-        	List<UserTable> list = UserTable.find.where().eq("username", username).findList();
-        	if (list.size()>=1){
-            	
-            	if (list.get(0).password.equals(password)){
-            		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            		System.out.println(df.getCalendar().toString());
-            		String hashkey = EncoderByMd5(df.getCalendar().toString()+username);
-            		return ok(hashkey);
-            	} else {
-            		return ok("101		密码错误 ");
-            	}
-            }else {
-            	return ok("102		用户不存在");
-            }
-        }catch (Exception e){
-        	return ok("102		用户不存在");
-        }
-        
-    }
+	@BodyParser.Of(BodyParser.Json.class)
+	public static Result login() {
+		
 
-    public static Result register() {
-        // return ok(index.render("MY new application is ready."));
-    	System.out.println("Here is the POST register process!");
-    	final Map<String, String[]> values = request().body().asFormUrlEncoded();
-        UserTable ut = new UserTable();
-        ut.username = values.get("username")[0];
-        ut.password = values.get("password")[0];
-        try{
-        	ut.save();
-        	return ok("注册成功!");
-        }catch (Exception e){
-        	return ok("103			用户名已存在");
-        }
-        
-    }
-  
+		// final Map<String, String[]> values =
+		// request().body().asFormUrlEncoded();
+		// final String username = values.get("username")[0];
+		// final String password = values.get("password")[0];
+		JsonNode json = request().body().asJson();
+		
+		System.out.println("Here is the POST login process!");
+		ObjectNode result = Json.newObject();
+		if (json == null) {	
+			
+			result.put("status", "104		找不到参数");
+			
+		} else {
+			
+			result = UserAuthentication.login(json);
+		}
+		if (result.get("status").equals("ok")){
+			return ok(result);
+		}else 
+			return badRequest(result);
+
+	}
+
+	public static Result register() {
+		// return ok(index.render("MY new application is ready."));
+		System.out.println("Here is the POST register process!");
+		final Map<String, String[]> values = request().body()
+				.asFormUrlEncoded();
+		UserTable ut = new UserTable();
+		ut.username = values.get("username")[0];
+		ut.password = values.get("password")[0];
+		try {
+			ut.save();
+			return ok("注册成功!");
+		} catch (Exception e) {
+			return ok("103			用户名已存在");
+		}
+
+	}
+
 }
--- LivePPT/app/models/UserAuthentication.java
+++ LivePPT/app/models/UserAuthentication.java
@@ -1,7 +1,65 @@
 package models;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.List;
+
+import models.mysql.UserTable;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ObjectNode;
+
+import play.libs.Json;
+import sun.misc.BASE64Encoder;
+
 public class UserAuthentication {
 	
+	public static String EncoderByMd5(String str)
+			throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		// 确定计算方法
+		MessageDigest md5 = MessageDigest.getInstance("MD5");
+		BASE64Encoder base64en = new BASE64Encoder();
+		// 加密后的字符串
+		String newstr = base64en.encode(md5.digest(str.getBytes("utf-8")));
+		return newstr;
+	}
 	
+	public static ObjectNode login(JsonNode json){
+		ObjectNode result = Json.newObject();
+		String username = json.findPath("username").getTextValue();
+		String password = json.findPath("password").getTextValue();
+		if (username == null) {
+			result.put("status", "102		用户不存在");
+		} else {
+			try {
+				List<UserTable> list = UserTable.find.where()
+						.eq("username", username).findList();
+				if (list.size() >= 1) {
+
+					if (list.get(0).password.equals(password)) {
+						long nt = new Date().getTime();
+						System.out.println(nt);
+						
+						String hashkey = EncoderByMd5(nt + username);
+						result.put("status", "OK");
+						result.put("access_token", hashkey);
+				
+					} else {
+						result.put("status", "101		密码错误 ");
+					}
+				} else {
+					result.put("status", "102		用户不存在");
+				}
+			} catch (Exception e) {
+				result.put("status", "102		用户不存在");
+			}
+		}
+		
+		
+		return result;
+		
+	}
 
 }
