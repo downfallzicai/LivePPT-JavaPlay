5cf00e59d0ebd24830f67787e9dba13834126389
WeiJie Li <vincent.is.lwj@gmail.com>
2013-02-25 12:05:51 +0800

    把POST信息返回值修改为状态码
    101   密码错误
    102   用户不存在
    103   用户名已存在

--- LivePPT/app/controllers/UserAction.java
+++ LivePPT/app/controllers/UserAction.java
@@ -2,9 +2,16 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import sun.misc.BASE64Encoder;
 
 import views.html.*;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -12,6 +19,16 @@ import models.mysql.UserTable;
 
 
 public class UserAction extends Controller {
+	
+	public String EncoderByMd5(String str) throws NoSuchAlgorithmException, 
+	UnsupportedEncodingException{
+	        //确定计算方法
+	        MessageDigest md5=MessageDigest.getInstance("MD5");
+	        BASE64Encoder base64en = new BASE64Encoder();
+	        //加密后的字符串
+	        String newstr=base64en.encode(md5.digest(str.getBytes("utf-8")));
+	        return newstr;
+	}
   
     public static Result index() {
         // return ok(index.render("MY new application is ready."));
@@ -30,15 +47,17 @@ public class UserAction extends Controller {
         	if (list.size()>=1){
             	
             	if (list.get(0).password.equals(password)){
+            		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            		System.out.println(df.getCalendar().toString());
             		return ok("Welcome back!"+ username);
             	} else {
-            		return ok("密码错误 !");
+            		return ok("101		密码错误 ");
             	}
             }else {
-            	return ok("用户名:"+username+"不存在.");
+            	return ok("102		用户不存在");
             }
         }catch (Exception e){
-        	return ok("用户名:"+username+"不存在.");
+        	return ok("102		用户不存在");
         }
         
     }
@@ -54,7 +73,7 @@ public class UserAction extends Controller {
         	ut.save();
         	return ok("注册成功!");
         }catch (Exception e){
-        	return ok("注册失败!"+e);
+        	return ok("103			用户名已存在");
         }
         
     }
